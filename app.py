3281c54d3efe3bee8c4e8e39b3a9bc2c4c951ecf
SIRSTN <stephane.velen@gmail.com>
2024-03-06 15:55:03 +0100

    cloud commit

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ api_key = os.environ.get(config.get('DEAL_RUN_VALUATION', 'APIKey'))
 api_secret = os.environ.get(config.get('DEAL_RUN_VALUATION', 'APISecret'))
 binance_client = Client(api_key, api_secret)
 
-def get_datetime_binance_price(crypto, datetime_str):
+def get_datetime_binance_price(keyword, datetime_str):
     symbol_map = {
         "Bitcoin": "BTCUSDT",
         "Ethereum": "ETHUSDT"
     }
-    symbol = symbol_map.get(crypto)
+    symbol = symbol_map.get(keyword)
     if not symbol:
-        raise ValueError(f"Invalid cryptocurrency: {crypto}")
+        raise ValueError(f"Invalid cryptocurrency: {keyword}")
 
     specific_time = datetime.fromisoformat(datetime_str)
     specific_time_ms = int(specific_time.timestamp() * 1000)  # Convert to milliseconds
@@ -48,7 +48,7 @@ def api_valuate_deals(keyword, date, price):
     except requests.RequestException as e:
         print(f"Error making request to the API: {e}")
 
-def valuate_crypto_for_2023(crypto):
+def valuate_crypto_for_2023(keyword):
     start_date = datetime(year=2023, month=1, day=1)
     end_date = datetime(year=2023, month=12, day=31)
     delta = timedelta(days=1)
@@ -57,17 +57,18 @@ def valuate_crypto_for_2023(crypto):
     while current_date <= end_date:
         # Format the date and explicitly set time to midnight without duplicating
         datetime_str = current_date.strftime('%Y-%m-%dT00:00:00')
-        print(f"Valuating {crypto} for {current_date.strftime('%d-%m-%Y')}...")
-        price = get_datetime_binance_price(crypto, datetime_str)
+        print(f"Valuating {keyword} for {current_date.strftime('%d-%m-%Y')}...")
+        price = get_datetime_binance_price(keyword, datetime_str)
         if price != "No data available for the specified time":
 
-            print(crypto)
+            print(keyword)
             print(current_date.strftime("%d-%m-%Y"))
             print(price)
 
-            api_valuate_deals(crypto, current_date.strftime("%d-%m-%Y"), float(price))
+            api_valuate_deals(keyword, current_date.strftime("%d-%m-%Y"), float(price))
         else:
             print(price)
         current_date += delta
 
 valuate_crypto_for_2023("Bitcoin")
+valuate_crypto_for_2023("Ethereum")
