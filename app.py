d271126290300e60fbdd1f4859f9545cc7ad4486
SIRSTN <stephane.velen@gmail.com>
2024-03-14 14:55:18 +0100

    cloud commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from binance.client import Client
 import os
 from configparser import ConfigParser
+from forex_python.converter import CurrencyRates
 
 # Load configuration file
 config = ConfigParser()
@@ -14,7 +15,7 @@ api_key = os.environ.get(config.get('DEAL_RUN_VALUATION', 'APIKey'))
 api_secret = os.environ.get(config.get('DEAL_RUN_VALUATION', 'APISecret'))
 binance_client = Client(api_key, api_secret)
 
-def get_datetime_binance_price(keyword, datetime_str):
+def get_binance_price(keyword, datetime_str):
     symbol_map = {
         "Bitcoin": "BTCUSDT",
         "Ethereum": "ETHUSDT"
@@ -30,10 +31,10 @@ def get_datetime_binance_price(keyword, datetime_str):
 
     if klines:
         close_price = klines[0][4]
-        return close_price
+        return float(close_price)
     else:
         return "No data available for the specified time"
-
+    
 def api_valuate_deals(keyword, date, price):
     api_url = "http://127.0.0.1:5000/Valuate_Deals"
     data = json.dumps({"keyword": keyword, "date": date, "price": price})
@@ -48,27 +49,19 @@ def api_valuate_deals(keyword, date, price):
     except requests.RequestException as e:
         print(f"Error making request to the API: {e}")
 
-def valuate_crypto_for_2023(keyword):
-    start_date = datetime(year=2023, month=1, day=1)
-    end_date = datetime(year=2023, month=12, day=31)
+def run_valuation(keyword):
+    start_date = datetime(year=2023, month=1, day=2)
+    end_date = datetime(year=2024, month=3, day=12)
     delta = timedelta(days=1)
 
     current_date = start_date
     while current_date <= end_date:
-        # Format the date and explicitly set time to midnight without duplicating
-        datetime_str = current_date.strftime('%Y-%m-%dT00:00:00')
         print(f"Valuating {keyword} for {current_date.strftime('%d-%m-%Y')}...")
-        price = get_datetime_binance_price(keyword, datetime_str)
-        if price != "No data available for the specified time":
-
-            print(keyword)
-            print(current_date.strftime("%d-%m-%Y"))
-            print(price)
-
-            api_valuate_deals(keyword, current_date.strftime("%d-%m-%Y"), float(price))
-        else:
-            print(price)
+        datetime_str = current_date.strftime('%Y-%m-%dT00:00:00')
+        price = get_binance_price(keyword, datetime_str)
+        api_valuate_deals(keyword, current_date.strftime("%d-%m-%Y"), price)
+        print(keyword, current_date.strftime("%d-%m-%Y"), price)
         current_date += delta
 
-valuate_crypto_for_2023("Bitcoin")
-valuate_crypto_for_2023("Ethereum")
+run_valuation("Bitcoin")
+run_valuation("Ethereum")
